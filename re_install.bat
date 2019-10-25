18b91250809be51873971eb50e167918bd4741e6
neo lin <lnonly@163.com>
2019-10-25 13:24:23 +0800

    MNT: version change

--- re_install.bat
+++ re_install.bat
@@ -4,6 +4,6 @@ python setup.py bdist_wheel --universal
 
 pip uninstall ps -y
 
-pip install dist\ps-0.1.5-py2.py3-none-any.whl
+pip install dist\ps-0.1.7-py2.py3-none-any.whl
 
-::twine upload --repository-url http://192.168.1.195:8071/repository/jasperpypi-hosted/ dist\ps-0.1.5-py2.py3-none-any.whl -u jasper -p jasper123
+::twine upload --repository-url http://192.168.1.195:8071/repository/jasperpypi-hosted/ dist\ps-0.1.7-py2.py3-none-any.whl -u jasper -p jasper123
