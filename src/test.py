a4b94b03e2106f03c7fb4a6fde3509096ec023a4
neo lin <lnonly@163.com>
2019-11-01 15:17:29 +0800

    MNT: version

--- src/test.py
+++ src/test.py
@@ -8,13 +8,13 @@ _logger = Logger(module_name_=__name__)
 
 if __name__ == "__main__":
     # calendar = TradeCalendarDB()
-    # date_list = calendar.get_trading_calendar(from_='20190913', to_='20190913')
+    # date_list = calendar.get_trading_calendar(from_='20191030', to_='20191031')
     # for d in date_list:
     #     _logger.info(f'Deal date {d}')
-    #     # download_daily_data(date_=d, is_overwrite_ = True)
-    #     daily_statistics(d)
-    # download_daily_data(date_='20191010', is_overwrite_ = True)
-    # daily_statistics('20191009')
+    #     download_daily_data(date_=d, is_overwrite_ = True)
+    #     daily_statistics(d, ['12_ARBT','82_ARBT',' 82_ZS'])
+    # download_daily_data(date_='20191031', is_overwrite_ = True)
+    daily_statistics('20191030')
     # daily_statistics('20191008',['90_JASON','82_JASON'])
     
     # init_db_info()
