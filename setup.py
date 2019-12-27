9574dd6ff4c227f3f4fb8143b6406a7d060502a8
neo lin <lnonly@163.com>
2019-12-27 10:07:01 +0800

    version control

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ps',
-    version='0.1.10',
+    version='0.1.13',
     description="performace statistics",
     author='Neo',
     author_email='neo.lin@jaspercapital.com',
--- src/test.py
+++ src/test.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from ps.calculation import daily_statistics
 from ps.summary import cal_monthly_performance
 from ps.init_data import download_daily_data, init_db_info
@@ -8,18 +10,19 @@ _logger = Logger(module_name_=__name__)
 
 if __name__ == "__main__":
     # calendar = TradeCalendarDB()
-    # date_list = calendar.get_trading_calendar(from_='20191030', to_='20191031')
+    # date_list = calendar.get_trading_calendar(from_='20191210', to_='20191224')
     # for d in date_list:
     #     _logger.info(f'Deal date {d}')
-    #     download_daily_data(date_=d, is_overwrite_ = True)
-    #     daily_statistics(d, ['12_ARBT','82_ARBT',' 82_ZS'])
-    # download_daily_data(date_='20191031', is_overwrite_ = True)
-    daily_statistics('20191030')
-    # daily_statistics('20191008',['90_JASON','82_JASON'])
+        # download_daily_data(date_=d, is_overwrite_ = False)
+        # daily_statistics(d)
+    # download_daily_data(date_='20191225', is_overwrite_ = True)
+    # daily_statistics('20191113', new_account=pd.DataFrame({'account_id':['98'], 'totalasset':[1990000]}))
+    daily_statistics('20191226')
     
     # init_db_info()
 
-    # bonus, defer_bonus = cal_monthly_performance(from_='20190701', to_='20190930')
+    # bonus, defer_bonus, sr = cal_monthly_performance(from_='20191101', to_='20191130')
     # bonus.to_csv(r'e:\temp\bonus.csv')
     # defer_bonus.to_csv(r'e:\temp\defer_bonus.csv')
+    # sr.to_csv(r'e:\temp\sr11.csv')
     pass
