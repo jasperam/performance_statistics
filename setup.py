f4bdaa83442b1efee268acb1d12ce3244e6b65ef
neo lin <lnonly@163.com>
2020-07-09 08:03:31 +0800

    MNT: version 0.1.22

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ps',
-    version='0.1.21',
+    version='0.1.22',
     description="performace statistics",
     author='Neo',
     author_email='neo.lin@jaspercapital.com',
